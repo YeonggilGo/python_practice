263b3eb19a72fd97fca071a693416af9c1da5bb8
YeonggilGo <dudrlf1859@naver.com>
2020-08-18 18:00:03 +0900

    0818

--- SWEA/D4/1233_4oper_check.py
+++ SWEA/D4/1233_4oper_check.py
@@ -6,15 +6,21 @@ def cal(oper, num1, num2):
         return num1 - num2
     elif oper == '*':
         return num1 * num2
-    else:
+    elif oper == '/':
         return num1 / num2
+    else:
+        return None
 
 
 def solve(info):
     if info[0].isdigit():
         return info[0]
     else:
-        return cal(info[0], solve(tree[int(info[1])-1]), solve(tree[int(info[2])-1]))
+        tmp = cal(info[0], solve(tree[int(info[1]) - 1]), solve(tree[int(info[2]) - 1]))
+        if tmp is None:
+            return None
+        else:
+            return tmp
 
 
 for tc in range(1, 11):
@@ -23,4 +29,9 @@ for tc in range(1, 11):
     for i in range(N):
         tree.append(input().split()[1:])
 
-    print(f'#{tc} {int(solve(tree[0]))}')
+    res = solve(tree[0])
+    if res is None:
+        res = 0
+    else:
+        res = 1
+    print(f'#{tc} {res}')
