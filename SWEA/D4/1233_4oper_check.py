ca9467ae3c654db67dd0db3aca158a3d04b8f32f
YeonggilGo <dudrlf1859@naver.com>
2020-08-19 18:50:38 +0900

    0819 1233

--- SWEA/D4/1233_4oper_check.py
+++ SWEA/D4/1233_4oper_check.py
@@ -1,37 +1,30 @@
-def cal(oper, num1, num2):
-    num1, num2 = float(num1), float(num2)
-    if oper == '+':
-        return num1 + num2
-    elif oper == '-':
-        return num1 - num2
-    elif oper == '*':
-        return num1 * num2
-    elif oper == '/':
-        return num1 / num2
+def find_last_node(idx):
+    if idx >= N:
+        last_node.add((idx - 1) // 2)
+        return
     else:
-        return None
-
-
-def solve(info):
-    if info[0].isdigit():
-        return info[0]
-    else:
-        tmp = cal(info[0], solve(tree[int(info[1]) - 1]), solve(tree[int(info[2]) - 1]))
-        if tmp is None:
-            return None
-        else:
-            return tmp
+        find_last_node(idx * 2 + 1)
+        find_last_node(idx * 2 + 2)
 
 
 for tc in range(1, 11):
     N = int(input())
     tree = []
     for i in range(N):
-        tree.append(input().split()[1:])
+        tree.append(input().split())
 
-    res = solve(tree[0])
-    if res is None:
-        res = 0
-    else:
-        res = 1
+    last_node = set()
+    find_last_node(0)
+    res = 1
+    for i in range(N):
+        if i in last_node:
+            print('마지막', i, tree[i][1])
+            if not tree[i][1].isdigit():
+                res = 0
+                break
+        else:
+            print(i, tree[i][1])
+            if tree[i][1].isdigit():
+                res = 0
+                break
     print(f'#{tc} {res}')
