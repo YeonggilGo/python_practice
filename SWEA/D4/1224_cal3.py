b9009925d40cbced017ebde98ec51064c0b2def5
YeonggilGo <dudrlf1859@naver.com>
2020-08-31 16:03:53 +0900

    0831

--- SWEA/D4/1224_cal3.py
+++ SWEA/D4/1224_cal3.py
@@ -1,49 +1,37 @@
-# 후위 계산, 후위로 만드는 것 모두 스택이다.
-
 for tc in range(1, 11):
     N = int(input())
-    Data = input()
+    data = input()
     stack = []
     num_lst = []
 
-    icp = {'*': 2, '+': 1, '(': 3}  # 넣을때
-    isp = {'*': 2, '+': 1, '(': 0}  # 스택안
+    push_prio = {'*': 2, '+': 1, '(': 3}
+    pop_prio = {'*': 2, '+': 1, '(': 0}
 
-    # Step 1: 중위 => 후위 표기법 변경
+    # Change to postfix
     for i in range(N):
-        print(stack,num_lst)
-        # 피연산자인 경우: 숫자 리스트 넣기
-        if Data[i].isdigit():
-            num_lst.append(Data[i])
+        if data[i].isdigit():
+            num_lst.append(data[i])
 
-        # 연산자인 경우
         else:
-            # stack이 빈 경우 => 무조건 append(여는 괄호의 case)
             if not stack:
-                stack.append(Data[i])
+                stack.append(data[i])
                 continue
 
-            # stack이 비지 않은 경우
-            elif stack:
-                # 닫는 괄호인 경우, 여는 괄호가 나올 때 까지 pop
-                if Data[i] == ')':
+            else:
+                if data[i] == ')':
                     while stack[-1] != '(':
                         num_lst.append(stack.pop())
                     stack.pop()
 
-                # icp & isp 비교
-                elif icp[Data[i]] > isp[stack[-1]]:
-                    stack.append(Data[i])
+                elif push_prio[data[i]] > pop_prio[stack[-1]]:
+                    stack.append(data[i])
 
                 else:
-                    # icp가 isp 보다 작으면 계속 pop & 연산자 리스트에 append
-                    while icp[Data[i]] <= isp[stack[-1]]:
+                    while push_prio[data[i]] <= pop_prio[stack[-1]]:
                         num_lst.append(stack.pop())
-                    stack.append(Data[i])
+                    stack.append(data[i])
 
-    print(num_lst)
 
-    # step 2: 계산
     for i in range(len(num_lst)):
         if num_lst[i].isdigit():
             stack.append(num_lst[i])
@@ -53,10 +41,10 @@ for tc in range(1, 11):
             num1 = int(stack.pop())
 
             if num_lst[i] == "+":
-                result = num1 + num2
+                tmp = num1 + num2
             elif num_lst[i] == "*":
-                result = num1 * num2
+                tmp = num1 * num2
 
-            stack.append(str(result))
+            stack.append(str(tmp))
 
     print(f'#{tc} {stack[0]}')
