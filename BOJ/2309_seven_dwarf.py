981551cf0558b4a6bc6448f6525ce05357d796e5
YeonggilGo <dudrlf1859@naver.com>
2020-09-18 18:00:25 +0900

    0918

---
+++ BOJ/2309_seven_dwarf.py
@@ -0,0 +1,15 @@
+height = [int(input()) for _ in range(9)]
+find = False
+for i in range(1 << 9):
+    temp = []
+    for j in range(10):
+        if i & (1 << j):
+            temp.append(height[j])
+        if len(temp) == 7 and sum(temp) == 100:
+            find = True
+            break
+    if find:
+        break
+
+for each in sorted(temp):
+    print(each)
