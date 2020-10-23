730af0d98652a85d355914f37f90f609007b99db
YeonggilGo <dudrlf1859@naver.com>
2020-10-23 18:09:24 +0900

    1023

--- SWEA/D3/1240_binary_pwd.py
+++ SWEA/D3/1240_binary_pwd.py
@@ -1,31 +1,32 @@
 T = int(input())
 
+dict_pwd = {
+    '0001101': '0', '0011001': '1',
+    '0010011': '2', '0111101': '3',
+    '0100011': '4', '0110001': '5',
+    '0101111': '6', '0111011': '7',
+    '0110111': '8', '0001011': '9'
+}
+
 for tc in range(1, T + 1):
     N, M = map(int, input().split())
 
-    pwd = []
+    sig = []
     for i in range(N):
-        pwd.append(input())
+        sig.append(input())
 
     pos = [0, 0]
 
-    for i in range(len(pwd)):
-        if '1' in pwd[i]:
+    for i in range(len(sig)):
+        if '1' in sig[i]:
             pos[0] = i
-            pos[1] = pwd[i].index('1')
+            pos[1] = sig[i].index('1')
             break
 
-    dict_pwd = {
-        '0001101': '0', '0011001': '1',
-        '0010011': '2', '0111101': '3',
-        '0100011': '4', '0110001': '5',
-        '0101111': '6', '0111011': '7',
-        '0110111': '8', '0001011': '9'
-    }
-    while pwd[pos[0]][pos[1]:pos[1] + 7] not in dict_pwd:
+    while sig[pos[0]][pos[1]:pos[1] + 7] not in dict_pwd:
         pos[1] -= 1
 
-    pwd = pwd[pos[0]][pos[1]:pos[1]+56]
+    pwd = sig[pos[0]][pos[1]:pos[1]+56]
     res = ''
 
     for i in range(8):
@@ -33,7 +34,7 @@ for tc in range(1, T + 1):
 
     ans = 0
     res = list(map(int, list(res)))
-    if (sum([res[i]*2 for i in range(len(res)-1) if i % 2 == 0]) + sum(res)) % 10 == 0:
+    if (sum([res[i]*3 if i % 2 == 0 else res[i] for i in range(8)])) % 10 == 0:
         ans = sum(res)
 
     print(f'#{tc} {ans}')
